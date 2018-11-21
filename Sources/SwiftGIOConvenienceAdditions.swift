0a7bab73c89acc5fa5d4d6141fe3f28db7d23608
Ron Pinz <ron.pinz@gmail.com>
2018-11-21 18:37:28 -0500

    removed redundant public declarations

--- Sources/SwiftGIOConvenienceAdditions.swift
+++ Sources/SwiftGIOConvenienceAdditions.swift
@@ -32,7 +32,7 @@ public extension CancellableProtocol {
     /// @callback is invoked.  This lifts a restriction in place for
     /// earlier GLib versions which now makes it easier to write cleanup
     /// code that unconditionally invokes e.g. g_cancellable_cancel().
-    public func connect<T>(data: UnsafePointer<T>? = nil, dataDestroyFunc: GLib.DestroyNotify? = nil, callback c: @escaping GLibObject.Callback) -> CUnsignedLong {
+    func connect<T>(data: UnsafePointer<T>? = nil, dataDestroyFunc: GLib.DestroyNotify? = nil, callback c: @escaping GLibObject.Callback) -> CUnsignedLong {
         let d = data.map { UnsafeMutableRawPointer(mutating: $0) } ?? UnsafeMutableRawPointer(ptr)
         let f = dataDestroyFunc ?? { _ in }
         return connect(callback: c, data: d, dataDestroyFunc: f)
