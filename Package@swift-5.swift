c7b00360d2d446b5ff4571409117f9442fcd6b49
Ron Pinz <ron.pinz@gmail.com>
2018-11-23 19:51:24 -0500

    add manifest for swift 5

---
+++ Package@swift-5.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "GIO",
+    products: [
+        .library(
+            name: "GIO",
+            targets: ["GIO"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/rpinz/SwiftGObject",
+            .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "GIO",
+            dependencies: ["GLibObject"],
+            path: "Sources"),
+        .testTarget(
+            name: "GIOTests",
+            dependencies: ["GIO"])
+    ],
+    swiftLanguageVersions: [
+        .v4_2
+    ]
+)
